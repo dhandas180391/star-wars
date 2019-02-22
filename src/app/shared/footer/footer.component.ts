909371334f2bde84eaf27f18d04dd00c33fe390a
Simmy Dhanda <simmyd91@gmail.com>
2019-02-22 20:46:33 +0000

    refactor(cleanup): page layout and styles cleanup

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+import { ModalService } from '../../modal/modal.service';
+
 @Component({
 	selector: 'sw-footer',
 	templateUrl: './footer.component.html'
 })
 export class FooterComponent {
 	public year = new Date();
+
+	constructor(private modalService: ModalService) { }
+
+	public viewCredits() {
+		return this.modalService.credits();
+	}
 }
